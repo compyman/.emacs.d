fd80791e5c09a26e17276485b58772bf18af7459
Nate Rosenbloom <nate@wave.com>
2020-04-01 10:15:05 -0400

    update

--- init.el
+++ init.el
@@ -28,35 +28,31 @@
 (unless (file-exists-p "~/.emacs.d/elpa/")
   (make-directory "~/.emacs.d/elpa"))
 (setq package-user-dir "~/.emacs.d/elpa/")
-		
 (add-to-list 'package-archives
-	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
+	     '("melpa" . "https://melpa.org/packages/") t)
 
 (mapc #'(lambda (package)
 	 (unless (package-installed-p package)
 	   (package-install package)))
       '(elpy
-        dart-mode
 	rainbow-delimiters
 	avy
 	exec-path-from-shell
-	company
-	rust-mode
-	racer
-	company-racer
-	company-go
 	go-eldoc
-	hungry-delete
 	paredit
 	magit
 	auctex
 	undo-tree
 	flycheck
 	flycheck-color-mode-line
-	flycheck-rust
 	slime
 	paradox
-	atom-dark-theme))
+	eink-theme
+        rust-mode
+        company-lsp
+        lsp-mode
+        lsp-ui
+        ein))
 
 ;;load each folder in the elpa directory
 (let ((default-directory "~/.emacs.d/elpa/"))
@@ -75,7 +71,7 @@
   (interactive)
   (save-excursion
     (mark-defun)
-    (indent-region (region-beginning) (region-end))))
+    (indent-nregion (region-beginning) (region-end))))
 
 
 ;;;; global key bindings
@@ -108,13 +104,11 @@
 
 ;;;; Flycheck Mode
 (after "flycheck-autoloads"
-  (setq-default flycheck-disabled-checkers '())
-  (append flycheck-disabled-checkers 'javascript-eslint)
   (add-hook 'c-mode-hook 'flycheck-mode)
   (add-hook 'c++-mode-hook 'flycheck-mode)
   (add-hook 'go-mode-hook 'flycheck-mode)
-  (add-hook 'rust-mode-hook 'flycheck-mode)
   (add-hook 'js-mode-hook 'flycheck-mode)
+  (add-hook 'rust-mode-hook 'flycheck-mode)
   (setq flycheck-clang-language-standard "c++11")
   (after "flycheck-color-mode-line-autoloads"
     (add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode)))
@@ -125,28 +119,20 @@
     (exec-path-from-shell-initialize)))
 ;;;; RUST MODE
 (after "rust-mode-autoloads"
-  (after "racer-autoloads"
-    (setq racer-cmd "~/builds/racer/target/release/racer")
-    (setq racer-rust-src-path "~/builds/rust/src")
-    (add-hook 'rust-mode-hook 'racer-mode)
-    (add-hook 'rust-mode-hook 'eldoc-mode))
-  (after "flycheck-autoloads"
-    (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
-  (set (make-local-variable 'company-backends)'(company-racer))
-  (local-set-key (kbd "M-.") 'racer-find-definition)) 
+  (add-hook 'rust-mode-hook 'rust-mode)
+  (after "lsp-mode-autoloads"
+    (add-hook 'rust-mode-hook 'lsp))
+  (after "lsp-ui-mode-autoloads"
+    (add-hook 'lsp-mode-hook 'lsp-ui-mode)))
 
 (after "company-autoloads"
   (global-company-mode)
   (setq company-idle-delay 0.2)
   (setq company-minimum-prefix-length 1)
-  (global-set-key (kbd "TAB") #'company-indent-or-complete-common)
-  (setq company-tooltip-align-annotations t))
-;;;; Go Mode
-(after "go-mode-autoloads"
-  (after "go-eldoc-autoloads"
-    (add-hook 'go-mode-hook 'go-eldoc-setup))
-  (add-hook 'before-save-hook 'gofmt-before-save)
-  (add-to-list 'company-backends 'company-go))
+  ;; (global-set-key (kbd "TAB") #'company-indent-or-complete-common)
+  (setq company-tooltip-align-annotations t)
+  (after "company-lsp-autoloads"
+    (push 'company-lsp company-backends)))
 
 ;;;;Org-mode
 (after "org-autoloads"
@@ -158,11 +144,6 @@
 (after "yasnippet-autoloads"
  (add-hook 'prog-mode-hook (lambda () (yas-minor-mode 1))))
 
-
-;;;;Hungry Delete Mode
-;; MOST IMPORTANT
-;; (after "hungry-delete-autoloads"
-;;   (global-hungry-delete-mode))
 ;;;;Undo Tree Mode
 (after "undo-tree-autoloads"
   (global-undo-tree-mode t)
@@ -189,12 +170,15 @@
 (after "ace-window-autoloads"
   (global-set-key (kbd "M-o") 'ace-window))
 
-;;; init.el ends here
+;;; paradox
+(after "paradox-autoloads"
+  (paradox-enable))
 
+;;; ein
 
-
-
-
+(after "ein-autoloads"
+  (require 'ein)
+  (require 'ein-notebook))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -202,25 +186,46 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("2642a1b7f53b9bb34c7f1e032d2098c852811ec2881eec2dc8cc07be004e45a0" "76dc63684249227d64634c8f62326f3d40cdc60039c2064174a7e7a7a88b1587" "e9460a84d876da407d9e6accf9ceba453e2f86f8b86076f37c08ad155de8223c" "78496062ff095da640c6bb59711973c7c66f392e3ac0127e611221d541850de2" "6a23db7bccf6288fd7c80475dc35804c73f9c9769ad527306d2e0eada1f8b466" "6dd2b995238b4943431af56c5c9c0c825258c2de87b6c936ee88d6bb1e577cb9" "c620ce43a0b430dcc1b06850e0a84df4ae5141d698d71e17de85e7494377fd81" "a1289424bbc0e9f9877aa2c9a03c7dfd2835ea51d8781a0bf9e2415101f70a7e" "32e3693cd7610599c59997fee36a68e7dd34f21db312a13ff8c7e738675b6dfc" "8e5dd88c42089566d5f8e1a23d3017c213eeccd94a7b9e1a58a2dc3e08cb26d5" "685a7460fdc4b8c38796234d3a96b3aacbe4fba739fb33b5d6d149051ce74a58" default)))
+    ("bffb799032a7404b33e431e6a1c46dc0ca62f54fdd20744a35a57c3f78586646" "21fb497b14820147b2b214e640b3c5ee19fcadc15bc288e3c16c9c9575d95d66" "2642a1b7f53b9bb34c7f1e032d2098c852811ec2881eec2dc8cc07be004e45a0" "76dc63684249227d64634c8f62326f3d40cdc60039c2064174a7e7a7a88b1587" "e9460a84d876da407d9e6accf9ceba453e2f86f8b86076f37c08ad155de8223c" "78496062ff095da640c6bb59711973c7c66f392e3ac0127e611221d541850de2" "6a23db7bccf6288fd7c80475dc35804c73f9c9769ad527306d2e0eada1f8b466" "6dd2b995238b4943431af56c5c9c0c825258c2de87b6c936ee88d6bb1e577cb9" "c620ce43a0b430dcc1b06850e0a84df4ae5141d698d71e17de85e7494377fd81" "a1289424bbc0e9f9877aa2c9a03c7dfd2835ea51d8781a0bf9e2415101f70a7e" "32e3693cd7610599c59997fee36a68e7dd34f21db312a13ff8c7e738675b6dfc" "8e5dd88c42089566d5f8e1a23d3017c213eeccd94a7b9e1a58a2dc3e08cb26d5" "685a7460fdc4b8c38796234d3a96b3aacbe4fba739fb33b5d6d149051ce74a58" default)))
+ '(ein:jupyter-default-server-command "")
+ '(ein:output-area-inlined-images t)
+ '(elpy-modules
+   (quote
+    (elpy-module-company elpy-module-eldoc elpy-module-flymake elpy-module-pyvenv elpy-module-highlight-indentation elpy-module-yasnippet elpy-module-django elpy-module-autodoc elpy-module-sane-defaults)))
+ '(elpy-project-ignored-directories
+   (quote
+    (".tox" "build" "dist" ".cask" ".ipynb_checkpoints" "test")))
+ '(elpy-rpc-virtualenv-path (quote current))
  '(fci-rule-color "#3E4451")
  '(flycheck-javascript-eslint-executable nil)
  '(global-display-line-numbers-mode t)
  '(indent-tabs-mode nil)
+ '(lsp-ui-flycheck-enable t)
  '(package-selected-packages
    (quote
-    (bicycle ace-window flycheck-yamllint yaml-mode geiser dart-mode emojify tuareg flymake-jslint wc-mode ini-mode json-mode ace-jump-mode elpy atom-one-dark-theme markdown-mode company-go go-eldoc company-racer company powerline go-mode avy atom-dark-theme moe-theme paradox slime exec-path-from-shell flycheck-color-mode-line undo-tree auctex magit paredit hungry-delete flycheck-rust racer rust-mode rainbow-delimiters)))
+    (ein org ess-R-data-view ess haskell-mode company-lsp lsp-mode lsp-ui rust-mode dash-alfred bicycle ace-window flycheck-yamllint yaml-mode geiser emojify tuareg flymake-jslint wc-mode ini-mode json-mode ace-jump-mode elpy atom-one-dark-theme markdown-mode go-eldoc powerline go-mode avy atom-dark-theme moe-theme paradox slime exec-path-from-shell flycheck-color-mode-line undo-tree auctex magit paredit hungry-delete flycheck-rust rainbow-delimiters)))
+ '(paradox-execute-asynchronously t)
  '(paradox-github-token t)
- '(safe-local-variable-values (quote ((elpy-project-root "./") (pyvenv-workon "remit"))))
+ '(paradox-spinner-type (quote moon))
+ '(safe-local-variable-values
+   (quote
+    ((elpy-project-root . "\\./")
+     (elpy-project-root . \./)
+     (elpy-project-root "./")
+     (pyvenv-workon "remit"))))
  '(show-paren-mode t)
  '(tool-bar-mode nil))
+;; (after "atom-dark-theme-autoloads"
+;;   (require 'atom-dark-theme)
+;;   (load-theme 'atom-dark))
+
+(after "eink-theme-autoloads"
+  (load-theme 'eink))
+
+(put 'narrow-to-region 'disabled nil)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#1d1f21" :foreground "#c5c8c6" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "nil" :family "Space Mono")))))
-(after "atom-dark-theme-autoloads"
-  (require 'atom-dark-theme)
-  (load-theme 'atom-dark))
-
+ )
